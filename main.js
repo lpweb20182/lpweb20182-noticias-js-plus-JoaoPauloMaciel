25611e05e9a666e9ecb0e646dfa49fef0a399d12
Joao Paulo Santos Maciel <joao.maciel@tre-to.gov.br>
2018-07-24 16:37:57 -0300

    Att Atividade do 1º dia de aula

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ function atualizarLista(noticia) {
         + '<p class="conteudo">'
         + noticia.conteudo
         + '<br>'
+        + '<p class="autor">'
+        + noticia.autor
+        + '<br>'
+        + '<p class="email">'
+        + noticia.email
+        + '<br>'
+        + '<p class="conteudo">'
+        + noticia.date
+        + '<br>'
+        + '<p class="conteudo">'
+        + noticia.time
+        + '<br>'
         + '<span>------------------</span>'
         + '<br>'
         + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
@@ -21,10 +33,18 @@ function atualizarLista(noticia) {
 function salvar(form) {
     var titulo = document.getElementById('frm-titulo').value;
     var conteudo = document.getElementById('frm-conteudo').value;
+    var autor = document.getElementById('frm-autor').value;
+    var email = document.getElementById('frm-email').email;
+    var date = document.getElementById('frm-datepublic').date;
+    var time = document.getElementById('frm-timepublic').time;
     var noticia = {
         id: noticias.length,
         titulo: titulo,
-        conteudo: conteudo
+        conteudo: conteudo,
+        autor: altor,
+        email: email,
+        date: date,
+        time: time
     };
     noticias.push(noticia);
     atualizarLista(noticia);
